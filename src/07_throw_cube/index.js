a27a0c8c2030c8945290e6b345485527d9715994
Kotaro Mochizuki <mochizuki@basicinc.jp>
2017-12-03 18:44:52 +0900

    Resize

--- src/07_throw_cube/index.js
+++ src/07_throw_cube/index.js
@@ -114,4 +114,11 @@ class Sample7 {
 
 }
 
+function onResize () {
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+}
+window.addEventListener('resize', onResize, false)
+
 module.exports =  Sample7
